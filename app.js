58643aa46fe740d5665de5e8cfc7d9614cdee7ab
nathaniellozano <lozanonl.nathaniel@gmail.com>
2021-09-03 09:37:07 -0500

    version 1.0

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const fs=require('fs')
-const port =3000
+const port =process.env.PORT || 3000
 /*
 const server = http.createServer(function(req,res){
    // res.write('Hello Node')
